0a5fd1d656ee40bbe5a2b302303024b88c6824aa
deox123 <nikolamilosevic93@yahoo.com>
2022-04-17 21:14:22 +0200

    add types

---
+++ places-fe/src/types.ts
@@ -0,0 +1,23 @@
+// TODO reuse types from BE
+export interface Place {
+	name: string;
+	address: string;
+	opening_hours: OpeningHours;
+}
+export interface OpeningHours {
+	days: Days;
+	closed_on_holidays: boolean;
+	open_by_arrangement: boolean;
+}
+export interface Days {
+	monday?: Day[] | null;
+	tuesday?: Day[] | null;
+	wednesday?: Day[] | null;
+	thursday?: Day[] | null;
+	friday?: Day[] | null;
+}
+export interface Day {
+	start: string;
+	end: string;
+	type: 'Open' | 'Closed';
+}
