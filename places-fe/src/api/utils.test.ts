f9c40c770c354a5d375cc47b5c740b6ab53851a4
deox123 <nikolamilosevic93@yahoo.com>
2022-04-22 19:32:51 +0200

    add utility for grouping days

---
+++ places-fe/src/api/utils.test.ts
@@ -0,0 +1,121 @@
+import { Days } from '../types';
+import { daysWithSameOpening, groupOpeningDays } from './utils';
+
+// TODO change mock data to be as minimal and clear as possible
+const mockDays1: Days = {
+	monday: [
+		{ start: '11:30', end: '14:00' },
+		{ start: '18:30', end: '22:00' },
+	],
+	tuesday: [
+		{ start: '11:30', end: '14:00' },
+		{ start: '18:30', end: '22:00' },
+	],
+	wednesday: [
+		{ start: '11:30', end: '14:00' },
+		{ start: '18:30', end: '22:00' },
+	],
+	thursday: [
+		{ start: '11:30', end: '14:00' },
+		{ start: '18:30', end: '22:00' },
+	],
+	friday: [
+		{ start: '11:30', end: '14:00' },
+		{ start: '18:30', end: '22:00' },
+	],
+};
+
+const mockDays2: Days = {
+	tuesday: [
+		{ start: '11:30', end: '15:00' },
+		{ start: '18:30', end: '00:00' },
+	],
+	wednesday: [
+		{ start: '11:30', end: '15:00' },
+		{ start: '18:30', end: '00:00' },
+	],
+	thursday: [
+		{ start: '11:30', end: '15:00' },
+		{ start: '18:30', end: '00:00' },
+	],
+	friday: [
+		{ start: '11:30', end: '15:00' },
+		{ start: '18:30', end: '00:00' },
+	],
+	saturday: [{ start: '18:00', end: '00:00' }],
+	sunday: [{ start: '11:30', end: '15:00' }],
+};
+
+describe('daysWithSameOpening', () => {
+	it('should return true if both days are undefined', () => {
+		const result = daysWithSameOpening(null, null);
+
+		expect(result).toEqual(true);
+	});
+
+	it('should return true if all openings are the same', () => {
+		const result = daysWithSameOpening(mockDays1.monday, mockDays1.tuesday);
+
+		expect(result).toEqual(true);
+	});
+
+	it('should return false if one opening start is different', () => {
+		const result = daysWithSameOpening(mockDays2.friday, mockDays2.saturday);
+
+		expect(result).toEqual(false);
+	});
+
+	it('should return false if one day is undefined', () => {
+		const result = daysWithSameOpening(mockDays1.friday, mockDays1.saturday);
+
+		expect(result).toEqual(false);
+	});
+
+	it('should return false if number of opening hours are not the same', () => {
+		const result = daysWithSameOpening(mockDays2.friday, mockDays2.saturday);
+
+		expect(result).toEqual(false);
+	});
+});
+
+describe('groupOpeningDays', () => {
+	it('should use the name of the leading day in the group', () => {
+		const result = groupOpeningDays(mockDays1);
+
+		expect(result).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({ label: expect.stringMatching(/^saturday/) }),
+			])
+		);
+	});
+
+	it('should use the name of the last day in the group', () => {
+		const result = groupOpeningDays(mockDays1);
+
+		expect(result).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({ label: expect.stringMatching(/sunday$/) }),
+			])
+		);
+	});
+
+	it('should not add dash to single day group', () => {
+		const result = groupOpeningDays(mockDays2);
+
+		expect(result).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({ label: expect.not.stringContaining('-') }),
+			])
+		);
+	});
+
+	it('should set empty array to non-open days', () => {
+		const result = groupOpeningDays(mockDays1);
+
+		expect(result).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({ openings: expect.arrayContaining([]) }),
+			])
+		);
+	});
+});
---
+++ places-fe/src/api/utils.ts
@@ -0,0 +1,72 @@
+import { Days, Day, GroupedDays } from '../types';
+
+const DAYS = [
+	'monday',
+	'tuesday',
+	'wednesday',
+	'thursday',
+	'friday',
+	'saturday',
+	'sunday',
+] as const;
+
+export const groupOpeningDays = (days: Days): GroupedDays[] => {
+	let start: string = DAYS[0];
+	let end: string = DAYS[0];
+	let grouped: GroupedDays[] = [];
+
+	DAYS.forEach((day, index) => {
+		if (!daysWithSameOpening(days[day], days[DAYS[index + 1]])) {
+			grouped.push({
+				label: generateLabel(start, end),
+				openings: days[day] ?? [],
+			});
+			start = DAYS[index + 1];
+			end = DAYS[index + 1];
+		} else {
+			end = DAYS[index];
+			if (!DAYS[index + 1]) {
+				grouped.push({
+					label: generateLabel(start, end),
+					openings: days[day] ?? [],
+				});
+			}
+		}
+	});
+
+	return grouped;
+};
+
+const generateLabel = (start: string, end: string): string => {
+	if (start === end) {
+		return start;
+	}
+	return start + ' - ' + end;
+};
+
+export const daysWithSameOpening = (
+	day1: Day[] | null | undefined,
+	day2: Day[] | null | undefined
+): boolean => {
+	if (!day1 && !day2) {
+		return true;
+	}
+
+	if ((!day1 && day2) || (day1 && !day2)) {
+		return false;
+	}
+
+	if (day1?.length !== day2?.length) {
+		return false;
+	}
+
+	return (day1 as Day[]).every((opening, index) => {
+		if (
+			opening.end === day2?.[index].end &&
+			opening.start === day2?.[index].start
+		) {
+			return true;
+		}
+		return false;
+	});
+};
