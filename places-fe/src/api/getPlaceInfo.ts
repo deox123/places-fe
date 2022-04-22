7a875cffd8454740814639d5b0a843a61f8bcc6b
deox123 <nikolamilosevic93@yahoo.com>
2022-04-22 19:29:40 +0200

    improve types

--- places-fe/src/api/getPlaceInfo.ts
+++ places-fe/src/api/getPlaceInfo.ts
@@ -1,4 +1,5 @@
+import { Place } from '../types';
 import { http } from './http';
 
-export const getPlaceInfo = (placeId: string) =>
+export const getPlaceInfo = (placeId: string): Promise<Place> =>
 	http.get('/place-info', { params: { placeId } }).then(res => res.data);
