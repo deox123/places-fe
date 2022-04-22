86f61a5f00b5313e3bae532f4c999d0044cba96f
deox123 <nikolamilosevic93@yahoo.com>
2022-04-22 21:25:21 +0200

    add todo test cases

---
+++ places-fe/src/App.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it.todo('should fire http request on button click');
+it.todo('should show place info on success');
+it.todo('should show error state on error');
+it.todo('should show loading state while loading');
